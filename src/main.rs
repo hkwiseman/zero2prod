04e53231027a9e85188fe506f1b17535285887a7
Kyle Wiseman <wisek414@icloud.com>
2023-03-11 17:06:43 -0500

    Added health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+async fn health_check() -> hyper::StatusCode {
+    hyper::StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health_check", get(health_check));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server.");
 }
