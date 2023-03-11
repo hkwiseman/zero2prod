14a54e32ba1b4fbf55d18b1f4b39f79eca840ac8
Kyle Wiseman <wisek414@icloud.com>
2023-03-11 17:11:19 -0500

    formmatting fix for health check endpoint commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
 async fn health_check() -> hyper::StatusCode {
@@ -13,7 +10,7 @@ async fn main() {
     let app = Router::new().route("/health_check", get(health_check));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
