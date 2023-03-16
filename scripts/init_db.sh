8ad0d6d4e376808b6d0fd4dc520eea6883e52a05
Kyle Wiseman <wisek414@icloud.com>
2023-03-16 18:20:39 -0400

    Fixed db script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -25,10 +25,9 @@ until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c "\q"
 	sleep 1
 done
 
->$2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
-DATABASE_URL=postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
-export DATABASE_URL
+export DATABASE_URL=postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
 
