ee937565a318b19f7b28b836031402e4984aafe8
Kyle Wiseman <wisek414@icloud.com>
2023-03-17 18:51:26 -0400

    Added secrets hiding, consistent logging, and telemetry module

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use crate::startup::AppState;
 use axum::{extract::State, Form};
 use chrono::Utc;
 use serde;
+use sqlx::{PgPool, postgres::PgQueryResult};
 use std::sync::Arc;
 use tracing::{error, info};
 use uuid::Uuid;
@@ -11,10 +12,40 @@ pub struct SubscribeForm {
     pub email: String,
     pub name: String,
 }
+#[tracing::instrument(
+    name = "saving new subscriber into the database",
+    skip(form, pool)
+    fields(
+        id = %request_id
+    )
+)]
+async fn insert_subscriber(
+    pool: &PgPool,
+    form: &SubscribeForm,
+    request_id: Uuid
+) -> Result<PgQueryResult, sqlx::Error> {
+    sqlx::query!(
+        r#"
+            INSERT INTO subscriptions (id, email, name, subscribed_at)
+            VALUES ($1, $2, $3, $4)
+            "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    
+}
 
 #[tracing::instrument(
-    name = "Saving new subscriber details into database",
-    skip(subscribe_user, state)
+    name = "Passing data to insert functionality",
+    skip(subscribe_user, state),
+    fields(
+        email = %subscribe_user.email,
+        name = %subscribe_user.name,
+    )
 )]
 pub async fn subscribe(
     State(state): State<Arc<AppState>>,
@@ -22,18 +53,7 @@ pub async fn subscribe(
 ) -> hyper::StatusCode {
     let request_id = Uuid::new_v4();
     info!("request_id {} - Adding '{}' '{}' as a new subscriber.", request_id, subscribe_user.name, subscribe_user.email);
-    let result = sqlx::query!(
-        r#"
-            INSERT INTO subscriptions (id, email, name, subscribed_at)
-            VALUES ($1, $2, $3, $4)
-            "#,
-        Uuid::new_v4(),
-        subscribe_user.email,
-        subscribe_user.name,
-        Utc::now()
-    )
-    .execute(&state.connection)
-    .await;
+    let result = insert_subscriber(&state.connection, &subscribe_user, request_id).await;
     match result {
         Ok(_) => {
             info!("request_id {} - Saved new subscriber details!", request_id);
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,18 +1,33 @@
 use std::net::{SocketAddr, TcpListener};
 
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::{
     configuration::{get_configuration, DatabaseSettings},
     routes::SubscribeForm,
+    telemetry::{get_subscriber, init_subscriber}
 };
+use once_cell::sync::Lazy;
 
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "name".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 pub struct TestApp {
     pub address: SocketAddr,
     pub db_pool: PgPool,
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
     let mut configuration = get_configuration().expect("Failed to read configuration");
 
     configuration.database.database_name = Uuid::new_v4().to_string();
@@ -34,7 +49,7 @@ async fn spawn_app() -> TestApp {
 }
 
 async fn config_db(config: &DatabaseSettings) -> PgPool {
-    let mut conn = PgConnection::connect(&config.connection_string_without_db())
+    let mut conn = PgConnection::connect(&config.connection_string_without_db().expose_secret())
         .await
         .expect("Failed to connect to database");
 
@@ -42,7 +57,7 @@ async fn config_db(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Unable to create database");
 
-    let pool = PgPool::connect(&config.connection_string())
+    let pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to database");
     sqlx::migrate!("./migrations")
